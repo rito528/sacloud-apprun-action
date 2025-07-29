9a6e977f6b6cfc52947246c45f49c24ce6860687
w-rin <w-rin@sakura.ad.jp>
2025-07-29 10:58:39 +0900

    first commit
    Signed-off-by: w-rin <w-rin@sakura.ad.jp>

---
+++ Dockerfile.go
@@ -0,0 +1,24 @@
+# Build stage
+FROM golang:1.24-alpine AS builder
+
+WORKDIR /app
+
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY . .
+
+RUN CGO_ENABLED=0 GOOS=linux go build -o app
+
+# Run stage
+FROM alpine:3.20
+
+WORKDIR /app
+
+COPY --from=builder /app/app /app/app
+
+ARG PORT=8080
+ENV PORT=$PORT
+EXPOSE $PORT
+
+ENTRYPOINT ["/app/app"]
